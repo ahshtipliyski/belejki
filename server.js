2496200d7a6062143929db024ae4540dc604db08
Anton Shtipliyski <antonshtipliyski@yahoo.com>
2020-10-21 21:59:07 -0300

    pre-deploy version

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const userRouter = require('./routes/userRouter')
 const noteRouter = require('./routes/noteRouter')
+const path = require('path')
 
 const app = express()
 app.use(express.json())
@@ -12,12 +13,6 @@ app.use(cors())
 //Routes
 app.use('/users', userRouter)
 app.use('/api/notes', noteRouter)
-
-//Listen Server
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log('Server is running on port', PORT)
-})
 
 //Connect to MongoDB
 
@@ -32,4 +27,16 @@ mongoose.connect(URI, {
   console.log('Connected to MongoDB')
 })
 
+// Must be below mongodb and above app.listen
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
+//Listen Server
+const PORT = process.env.PORT || 5000 
+app.listen(PORT, () => {
+  console.log('Server is running on port', PORT)
+})
